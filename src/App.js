51cd118f69280be6e5d9bf0346c9a10ede64df7c
JADHAV SACHIN RAJHANS <srjadhav886@gmail.com>
2022-01-04 19:19:20 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -10,16 +10,39 @@ export default function Instagram() {
 }
 
 function Navbar() {
-  const [message, setmsg] = useState([]);
-  const [msg, setMsg] = useState("");
+  let [msg, setMsg] = useState("");
 
   const changeMessage = (e) => {
     setMsg(e.target.value);
   };
 
-  const messages = () => {
-    const newmessagelist = [msg, ...message];
-    setmsg(newmessagelist);
+  let [msg1, setMsg1] = useState("");
+
+  const changeMessage1 = (e) => {
+    setMsg1(e.target.value);
+  };
+  let [msg2, setMsg2] = useState("");
+
+  const changeMessage2 = (e) => {
+    setMsg2(e.target.value);
+  };
+  let [msg3, setMsg3] = useState("");
+
+  const changeMessage3 = (e) => {
+    setMsg3(e.target.value);
+  };
+
+  const [userList, setUserList] = useState([]);
+  const register = () => {
+    const user = {
+
+      Firstname: msg,
+      Middlename: msg1,
+      Lastname: msg2,
+      Address: msg3,
+    };
+    const newList = [user, ...userList];
+    setUserList(newList);
   }
 
   return (
@@ -46,15 +69,39 @@ function Navbar() {
           </div>
         </div>
       </nav>
-      <div>
-        <h1>Input Operation</h1>
-        <input type="text" placeholder='type something' value={msg} onChange={changeMessage} />
-        <input type="button" value="Enter" onClick={messages} />
+      <h1 className='mt-3'>Personal Information</h1>
+      <div className="row mt-2">
+        <div className="col">
+          <input type="text" className="form-control" placeholder="First name" aria-label="First name" value={msg} onChange={changeMessage} />
+        </div>
+        <div className="col">
+          <input type="text" className="form-control" placeholder="Middle name" aria-label="Middle name" value={msg1} onChange={changeMessage1} />
+        </div>
+        <div className="col">
+          <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" value={msg2} onChange={changeMessage2} />
+        </div>
       </div>
-      {message.map((item, index) =>
-        <div className='mt-1' key={index}>{item}</div>
-      )
-      }
+      <div className="row mt-5">
+        <div className="col-6 ">
+          <input type="text" className="form-control form-control-lg" placeholder="Address" aria-label="Address" value={msg3} onChange={changeMessage3} />
+        </div>
+      </div>
+
+      <div class="mb-3 mt-2">
+        <button class="btn btn-primary" type="submit" onClick={register}>Submit form</button>
+      </div>
+
+      <hr />
+      <div>
+        {userList.map((item, index) => (
+          <div key={index}>
+            <br /> <br />
+            {item.Firstname} {item.Middlename} {item.Lastname} <br /> <br />{item.Address}
+          </div>
+        ))}
+      </div>
+
+
 
     </>
   );
