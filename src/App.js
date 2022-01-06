c610a90ec65ed921584d9505e2bd528a75eeb92d
JADHAV SACHIN RAJHANS <srjadhav886@gmail.com>
2022-01-06 21:20:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,108 +1,128 @@
-import './App.css'
-import { useState } from "react";
-export default function Instagram() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <MyComponent />
     </>
-
   );
 }
 
-function Navbar() {
-  let [msg, setMsg] = useState("");
+function MyComponent() {
+  const [username, setUsername] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [list, setList] = useState([]);
 
-  const changeMessage = (e) => {
-    setMsg(e.target.value);
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleemailChange = (e) => {
+    setemail(e.target.value);
   };
 
-  let [msg1, setMsg1] = useState("");
+  const addUser = async () => {
+    if (username == "" || password == "" || email == "") {
+      alert("Validation fails");
+      return;
+    }
 
-  const changeMessage1 = (e) => {
-    setMsg1(e.target.value);
-  };
-  let [msg2, setMsg2] = useState("");
-
-  const changeMessage2 = (e) => {
-    setMsg2(e.target.value);
-  };
-  let [msg3, setMsg3] = useState("");
-
-  const changeMessage3 = (e) => {
-    setMsg3(e.target.value);
-  };
-
-  const [userList, setUserList] = useState([]);
-  const register = () => {
-    const user = {
-
-      Firstname: msg,
-      Middlename: msg1,
-      Lastname: msg2,
-      Address: msg3,
+    const url = "http://localhost:4000/us";
+    const data = {
+      username: username,
+      password: password,
+      email: email,
     };
-    const newList = [user, ...userList];
-    setUserList(newList);
-  }
+
+    // AJAX using AXIOS
+    await axios.post(url, data);
+
+    const newList = [data, ...list];
+    setList(newList);
+
+    setUsername("");
+    setPassword("");
+    setemail("");
+  };
+
+  const getUser1 = async () => {
+    const url = "http://localhost:4000/users";
+    const result = await axios.get(url);
+
+    const list = result.data;
+    const newList = [...list];
+    setList(newList);
+  };
+
+  const getUser = async () => {
+    const url = "http://localhost:4000/users";
+    const result = await fetch(url);
+    const list = await result.json();
+
+    const newList = [...list];
+    setList(newList);
+  };
+
+  // LIke Constructor
+  useEffect(() => getUser(), []);
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">AMAZE TV</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">MOVIES</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-      <h1 className='mt-3'>Personal Information</h1>
-      <div className="row mt-2">
-        <div className="col">
-          <input type="text" className="form-control" placeholder="First name" aria-label="First name" value={msg} onChange={changeMessage} />
-        </div>
-        <div className="col">
-          <input type="text" className="form-control" placeholder="Middle name" aria-label="Middle name" value={msg1} onChange={changeMessage1} />
-        </div>
-        <div className="col">
-          <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" value={msg2} onChange={changeMessage2} />
-        </div>
-      </div>
-      <div className="row mt-5">
-        <div className="col-6 ">
-          <input type="text" className="form-control form-control-lg" placeholder="Address" aria-label="Address" value={msg3} onChange={changeMessage3} />
-        </div>
-      </div>
-
-      <div class="mb-3 mt-2">
-        <button class="btn btn-primary" type="submit" onClick={register}>Submit form</button>
-      </div>
-
-      <hr />
+    <div>
+      <h2 className="bg-dark text-light p-3">User Registration</h2>
       <div>
-        {userList.map((item, index) => (
-          <div key={index}>
-            <br /> <br />
-            {item.Firstname} {item.Middlename} {item.Lastname} <br /> <br />{item.Address}
-          </div>
-        ))}
+        <input
+          className="form-control form-control-lg mb-1"
+          type="text"
+          name=""
+          id=""
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Enter Username"
+        />
+      </div>
+      <div>
+        <input
+          className="form-control form-control-lg mb-1"
+          type="text"
+          name=""
+          id=""
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Enter Password"
+        />
+      </div>
+      <div>
+        <input
+          className="form-control form-control-lg mb-1"
+          type="text"
+          name=""
+          id=""
+          value={email}
+          onChange={handleemailChange}
+          placeholder="email"
+        />
+      </div>
+      <div>
+        <input
+          className="btn btn-secondary w-100"
+          type="button"
+          name=""
+          value="Register"
+          onClick={addUser}
+        />
       </div>
 
+      <h3 className="bg-dark text-light mt-1 p-3">User List</h3>
 
-
-    </>
+      {list.map((item, index) => (
+        <div key={index} className="alert alert-secondary fs-4">
+          {item.username} {item.password}{item.email}
+        </div>
+      ))}
+    </div>
   );
 }
